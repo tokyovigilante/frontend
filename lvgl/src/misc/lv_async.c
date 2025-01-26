963e0e80215b2f8f5445bbce653bac913c5ad803
Ryan Walklin <ryan@testtoast.com>
2025-01-26 15:44:57 +1300

    Add LVGL v9 source

--- lvgl/src/misc/lv_async.c
+++ lvgl/src/misc/lv_async.c
@@ -8,8 +8,8 @@
  *********************/
 
 #include "lv_async.h"
-#include "lv_mem.h"
-#include "lv_timer.h"
+#include "lv_timer_private.h"
+#include "../stdlib/lv_mem.h"
 
 /*********************
  *      DEFINES
@@ -21,14 +21,14 @@
 
 typedef struct _lv_async_info_t {
     lv_async_cb_t cb;
-    void *user_data;
+    void * user_data;
 } lv_async_info_t;
 
 /**********************
  *  STATIC PROTOTYPES
  **********************/
 
-static void lv_async_timer_cb(lv_timer_t *timer);
+static void lv_async_timer_cb(lv_timer_t * timer);
 
 /**********************
  *  STATIC VARIABLES
@@ -42,45 +42,47 @@ static void lv_async_timer_cb(lv_timer_t *timer);
  *   GLOBAL FUNCTIONS
  **********************/
 
-lv_res_t lv_async_call(lv_async_cb_t async_xcb, void *user_data) {
+lv_result_t lv_async_call(lv_async_cb_t async_xcb, void * user_data)
+{
     /*Allocate an info structure*/
-    lv_async_info_t *info = lv_mem_alloc(sizeof(lv_async_info_t));
+    lv_async_info_t * info = lv_malloc(sizeof(lv_async_info_t));
 
-    if (info == NULL)
-        return LV_RES_INV;
+    if(info == NULL)
+        return LV_RESULT_INVALID;
 
     /*Create a new timer*/
-    lv_timer_t *timer = lv_timer_create(lv_async_timer_cb, 0, info);
+    lv_timer_t * timer = lv_timer_create(lv_async_timer_cb, 0, info);
 
-    if (timer == NULL) {
-        lv_mem_free(info);
-        return LV_RES_INV;
+    if(timer == NULL) {
+        lv_free(info);
+        return LV_RESULT_INVALID;
     }
 
     info->cb = async_xcb;
     info->user_data = user_data;
 
     lv_timer_set_repeat_count(timer, 1);
-    return LV_RES_OK;
+    return LV_RESULT_OK;
 }
 
-lv_res_t lv_async_call_cancel(lv_async_cb_t async_xcb, void *user_data) {
-    lv_timer_t *timer = lv_timer_get_next(NULL);
-    lv_res_t res = LV_RES_INV;
+lv_result_t lv_async_call_cancel(lv_async_cb_t async_xcb, void * user_data)
+{
+    lv_timer_t * timer = lv_timer_get_next(NULL);
+    lv_result_t res = LV_RESULT_INVALID;
 
-    while (timer != NULL) {
+    while(timer != NULL) {
         /*Find the next timer node*/
-        lv_timer_t *timer_next = lv_timer_get_next(timer);
+        lv_timer_t * timer_next = lv_timer_get_next(timer);
 
         /*Find async timer callback*/
-        if (timer->timer_cb == lv_async_timer_cb) {
-            lv_async_info_t *info = (lv_async_info_t *) timer->user_data;
+        if(timer->timer_cb == lv_async_timer_cb) {
+            lv_async_info_t * info = (lv_async_info_t *)timer->user_data;
 
             /*Match user function callback and user data*/
-            if (info->cb == async_xcb && info->user_data == user_data) {
-                lv_timer_del(timer);
-                lv_mem_free(info);
-                res = LV_RES_OK;
+            if(info->cb == async_xcb && info->user_data == user_data) {
+                lv_timer_delete(timer);
+                lv_free(info);
+                res = LV_RESULT_OK;
             }
         }
 
@@ -94,9 +96,13 @@ lv_res_t lv_async_call_cancel(lv_async_cb_t async_xcb, void *user_data) {
  *   STATIC FUNCTIONS
  **********************/
 
-static void lv_async_timer_cb(lv_timer_t *timer) {
-    lv_async_info_t *info = (lv_async_info_t *) timer->user_data;
+static void lv_async_timer_cb(lv_timer_t * timer)
+{
+    /*Save the info because an lv_async_call_cancel might delete it in the callback*/
+    lv_async_info_t * info = (lv_async_info_t *)timer->user_data;
+    lv_async_info_t info_save = *info;
+    lv_timer_delete(timer);
+    lv_free(info);
 
-    info->cb(info->user_data);
-    lv_mem_free(info);
+    info_save.cb(info_save.user_data);
 }
